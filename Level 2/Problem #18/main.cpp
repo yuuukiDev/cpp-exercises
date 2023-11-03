6589576e5a733d2f05a2c6b55d734737a1c4d8e6
yuuukiDev <mohamedemadeldin34@gmail.com>
2023-11-03 18:47:45 +0200

    Print 3 random numbers from 1 to 10 is solved

--- Level 2/Problem #18/main.cpp
+++ Level 2/Problem #18/main.cpp
@@ -1,4 +1,18 @@
 #include <iostream>
+#include <cstdlib>
 
 
+using namespace std;
 
+
+void randomNumberFrom1To10() {
+    int n = 10;
+    srand(time(0));
+    for(int i = 0; i < 3; i++) {
+        cout << rand() % n << endl;
+    }
+}
+
+int main () {
+    randomNumberFrom1To10();
+}
